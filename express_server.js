c8a9ac7a2298f3c7f80fe8e294225328c7f0742b
Stacey Keating <staceyamberkk@gmail.com>
2020-04-14 00:26:07 +0000

    added longURL info in tinyURL field on :shortURL pages

--- express_server.js
+++ express_server.js
@@ -29,7 +29,7 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL};
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 })
 
