9a0a7e86d13bf7ecd4d5c04f8c120a005502fabb
Stacey Keating <staceyamberkk@gmail.com>
2020-04-14 03:09:47 +0000

    short url link to longurl is working

--- express_server.js
+++ express_server.js
@@ -36,14 +36,24 @@ app.get("/urls/new", (req, res) => {
 
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;  // Log the POST request body to the console
-  res.redirect(`/urls/${shortURL}`)       // Respond with 'Ok' (we will replace this)
+  urlDatabase[shortURL] = req.body.longURL;  
+  res.redirect(`/urls/${shortURL}`)  
+
+
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});   
+
+
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 })
+
 
 
 app.listen(PORT, () => {
